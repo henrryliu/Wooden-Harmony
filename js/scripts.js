107ca3120e2a76918a71d8aab1b776cfddc0195f
Liu Henrry <141622365+henrryliu@users.noreply.github.com>
2023-12-15 20:36:58 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
+
+function search() {
+    var query = document.getElementById('query').value;
+    var url = 'https://script.google.com/macros/s/AKfycbzAI3qKu6zy4hYOykA31MxZvdOmXVJshRfUTQGyoD5Tmh-Uun9NEM8zUTlkKF9YrfJk/exec?q=' + encodeURIComponent(query);
+    fetch(url)
+        .then(response => response.text())
+        .then(text => document.getElementById('result').innerText = text)
+        .catch(error => console.error('Error:', error));
+}
